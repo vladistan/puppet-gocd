b1b02d6e347798f409fc3c4e617deac2dcd5812b
Vlad <vlad@v-lad.org>
2016-09-09 21:09:51 -0400

    Revert "Switch to puppet/archive for downloading plugins."
    This reverts commit 6eb5b4c8ea83c1699a9aba4dcd3b8556c83d587b.

--- manifests/server/plugin.pp
+++ manifests/server/plugin.pp
@@ -16,26 +16,34 @@
 #
 
 define gocd::server::plugin (
-  $source,
+  $url,
 ) {
+
+  # wget from https://github.com/maestrodev/puppet-wget
+  include wget
+
   validate_string($name)
-  validate_string($source)
+  validate_string($url)
 
   $name_regex = '^[a-z0-9_-]{3,32}$'
   $name_error = "The plugin name '${name}' is not valid."
+
+
   validate_re($name, $name_regex, $name_error)
 
-  archive { "/var/lib/go-server/plugins/external/${name}.jar":
-    ensure  => present,
-    source  => $source,
-    extract => false,
-  } ->
+  $download_dir = '/tmp'
 
-  file { "/var/lib/go-server/plugins/external/${name}.jar":
-    ensure => present,
+
+  $tmpzip = "${download_dir}/${name}.zip"
+
+  wget::fetch { "s3-plugin-dl-${name}":
+        source      => $url,
+        destination => $tmpzip,
+  }  ->
+  file { $name:
+    ensure => file,
+    path   => "/var/lib/go-server/plugins/external/${name}.jar",
+    source => $tmpzip,
     notify => Service[$::gocd::server::service_name],
-    mode   => '0644',
-    owner  => 'go',
-    group  => 'go',
   }
 }
